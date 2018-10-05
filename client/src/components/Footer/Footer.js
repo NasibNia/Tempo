610cf634d4a7fa06a64f5ee942bf0cf1072c1faa
Sajeel Malik <sajeel23@berkeley.edu>
2018-10-04 23:58:00 -0700

    evaluate nav configuration

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
 
-class Footer extends Component{
+import "./Footer.css"
+
+const styles = {
+    footer: {
+        position: "relative",
+        height: "25vh",
+        width: "100%",
+        margin: "20px 0 0 0",
+        backgroundImage: "linear-gradient(#010131, #000022)",
+        color: "rgb(195, 200, 240)"
+    }
+}
+
+class Footer extends Component {
 
     render() {
 
-        return(
-        <footer>
-            Eat my asparagus.
-        </footer>
+        return (
+            <Grid container direction="row" justify="center" alignItems="flex-start" style={styles.footer}>
+                <p id="footer-text"><span ><a id="github" href="https://github.com/NasibNia/Tempo"><i class="fab fa-github"></i> Github </a>|</span> Developed in San Francisco, CA © 2018 Tempo</p>
+            </Grid>
         )
     }
-    
+
 }
 
-export default Footer;
+export default withStyles(styles)(Footer);
