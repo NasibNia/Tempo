4c5139ea8b873c635a9de9c649de5adadec1536a
Sajeel Malik <sajeel23@berkeley.edu>
2018-10-15 11:28:21 -0700

    fixing login errors

--- client/src/pages/VenueHome/VenueHome.jsx
+++ client/src/pages/VenueHome/VenueHome.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import {Redirect} from 'react-router-dom';
+
 import Body from "../../components/Body";
 
 import API from "../../utils/API";
@@ -24,32 +24,28 @@ class VenueHome extends Component {
         name: "",
         description: "",
         statistics: "",
-        user: "venue"
-      };
-    
-      componentDidMount() {
-        // this.loadBands();
-        // console.log("params", this.props.match)
-      }
-    
-    //   loadBands = () => {
-    //     API.getBands()
-    //       .then(res =>
-    //         this.setState({ bands: res.data, name: "", description: "", statistics: "" })
-    //       )
-    //       .catch(err => console.log(err));
-    //   };
+        user: "venue",
+        loggedIn: true
+    };
+
+    componentDidMount() {
+        API.getUser().then(res => {
+            console.log("component mounting check", res.data);
+            if (!res.data.user) {
+              this.setState({ loggedIn: false });
+            //   window.location.href = "/signin";
+            } else {
+              this.setState({ loggedIn: true });
+              // this.loadShows(res.data.user.id);
+            }
+          });
+    }
+
 
     render() {
 
         return (
             <div>
-                {/* <Header />
-                <Grid container direction="row" justify="center" alignItems="flex-start" spacing={24} style={{ marginTop: "5%" }}>
-                    <Nav className = {styles.column}/>
-                    <Main />
-                    <Shows className = {styles.column}/>
-                </Grid> */}
                 <Body userType={this.state.user} url={this.props.match} />
             </div>
         )
