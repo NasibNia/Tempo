50bb2f0153be3907d9ceb387a83a08ce9a0d8e4a
Atton <jabusau@gmail.com>
2018-10-15 01:46:04 -0700

    fixed comment issue

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-// const venueRoutes = require('./venues');
+const venueRoutes = require('./venues');
 const bandRoutes = require('./bands');
 const showRoutes = require('./shows');
 const ratingRoutes = require('./ratings');
 
 
 
-// router.use("/venues", venueRoutes);
+router.use("/venues", venueRoutes);
 router.use("/bands", bandRoutes);
 router.use("/shows", showRoutes);
 router.use("/ratings", ratingRoutes);
