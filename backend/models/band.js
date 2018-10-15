1a479d266fad1a366e236ef64cf020f2da789fe0
Sajeel Malik <sajeel23@berkeley.edu>
2018-10-14 20:42:37 -0700

    trying to add show

--- backend/models/band.js
+++ backend/models/band.js
@@ -42,7 +42,7 @@ module.exports = function(sequelize , DataTypes){
             allowNull: true
         },
         profilePic : {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: true
         },
         readyToGig : {
