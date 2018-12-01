d6946c1f4018270e3bdb5e0339051ff12d161a4f
Sajeel Malik <sajeel23@berkeley.edu>
2018-11-30 20:19:21 -0800

    add searching functionality to header

--- client/src/components/HeaderBar/HeaderBar.jsx
+++ client/src/components/HeaderBar/HeaderBar.jsx
@@ -1,56 +1,73 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { Redirect } from 'react-router-dom';
-import "./HeaderBar.css";
 import { Link } from 'react-router-dom'
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
 import API from "../../utils/API.js";
 import axios from 'axios'
 
+import "./HeaderBar.css";
+
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+})
 
 class HeaderBar extends Component {
 
 
-  state ={
-    loggedIn : false
+  state = {
+    loggedIn: false,
+    search: ""
   };
 
 
 
   componentDidMount() {
-      API.getUser(
-        // function(err,data){
-        //   console.log(data)
-        // }
-        )
-        .then(res => {
-          console.log("header bar component mounting check", res.data.user);
-          if (res.data.user) {
-                this.setState({loggedIn : true});
-              }
-              console.log("state ", this.state.loggedIn)    
+    API.getUser(
+      // function(err,data){
+      //   console.log(data)
+      // }
+    )
+      .then(res => {
+        console.log("header bar component mounting check", res.data.user);
+        if (res.data.user) {
+          this.setState({ loggedIn: true });
+        }
+        console.log("state ", this.state.loggedIn)
 
-        });
-      }
-    // const user = API.getUser(
-    //   function(err,data){
-    //   console.log(data)
-    // });
-    
-    //   console.log("header getUser: ", user)
-    //   if (!user) {
-    //     this.setState({loggedIn : false});
-    //   }
-    
-    // }
-    
-  
+      });
+  }
+  // const user = API.getUser(
+  //   function(err,data){
+  //   console.log(data)
+  // });
 
-  handleLogout  = event =>{
+  //   console.log("header getUser: ", user)
+  //   if (!user) {
+  //     this.setState({loggedIn : false});
+  //   }
+
+  // }
+
+  handleInputChange = event => {
+
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+
+  };
+
+
+  handleLogout = event => {
     event.preventDefault();
-      API.logout()
-      .then(res=>{
+    API.logout()
+      .then(res => {
         console.log("res   from logout route   ", res);
-        if (res.data.logout){
+        if (res.data.logout) {
           window.location.href = "/signin";
           // return <Redirect to='/signin'/>
         } else {
@@ -70,9 +87,26 @@ class HeaderBar extends Component {
   render(props) {
     // if (!this.state.loggedIn)
     //   return <Redirect to='/signin'/>
+    const { classes } = this.props;
 
     let button;
     let headerIcon;
+    let search =
+      <TextField
+        id="search-input"
+        label="Email"
+        className={classes.textField}
+        type="text"
+        name="search"
+        autoComplete="search"
+        margin="normal"
+        variant="filled"
+        onChange={this.handleInputChange}
+        value={this.state.search}
+        // helperText={this.state.errors.email}
+      />
+
+      ;
 
     if (this.state.loggedIn) {
       button = <div className="log-out-button">
@@ -83,9 +117,9 @@ class HeaderBar extends Component {
           LOG OUT
     </Button>
       </div>
-      headerIcon =  <div className="header-item">
-                <div className="header-icon"></div>
-                </div>
+      headerIcon = <div className="header-item">
+        <div className="header-icon"></div>
+      </div>
     } else {
       button = <div className="log-out-button">
         <Button variant="contained"
@@ -107,6 +141,7 @@ class HeaderBar extends Component {
             <h4>{this.props.userType} suite</h4>
           </div>
           <div className="header-items">
+          {search}
             {headerIcon}
             {headerIcon}
             {headerIcon}
@@ -118,4 +153,4 @@ class HeaderBar extends Component {
   }
 }
 
-export default HeaderBar;
+export default withStyles(styles)(HeaderBar);
