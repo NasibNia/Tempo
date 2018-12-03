ed20b00a1e023245f96590deb192ffcdcb15af77
NasibNia <nasibnia@gmail.com>
2018-12-02 23:29:33 -0800

    deploy4 - attepmt 5 back

--- backend/server.js
+++ backend/server.js
@@ -49,9 +49,9 @@ app.use(express.json());
 const path = require ('path');
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    // app.get("/*", function(req, res) {
-    //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    // });
+    app.get("/", function(req, res) {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
   }
   /*
   else {
