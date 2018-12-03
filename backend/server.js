81ca940ffbffdc4f39acf2a0c768b83c887f93ca
NasibNia <nasibnia@gmail.com>
2018-12-03 10:40:26 -0800

    deploy4 - attepmt 5 bring login back to  server routes

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ app.use("/",routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 
