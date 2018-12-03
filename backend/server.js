a22b5cf413fe241c51f91409e8116900d0bf5703
NasibNia <nasibnia@gmail.com>
2018-12-03 14:44:17 -0800

    deploy4 - attepmt 5 bring login back to  server routes

--- backend/server.js
+++ backend/server.js
@@ -64,12 +64,12 @@ const path = require ('path');
 app.use(passport.initialize()); // initialize passport
 app.use(passport.session()); // persistent login sessions
 const routes = require ("./routes/index.js");
-app.use("/",routes);
+app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
