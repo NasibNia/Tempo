ec3619cfd505793182345678d3fe704c3e01d82b
NasibNia <nasibnia@gmail.com>
2018-12-02 21:00:39 -0800

    deploy4 - attepmt 3

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
+    app.use(express.static("client/build"));
 }
 
 app.use(passport.initialize()); // initialize passport
