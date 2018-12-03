c8202516081b390cbc254725de38c39633102ab5
NasibNia <nasibnia@gmail.com>
2018-12-02 23:21:12 -0800

    deploy4 - attepmt 5 back

--- backend/server.js
+++ backend/server.js
@@ -49,18 +49,18 @@ app.use(express.json());
 const path = require ('path');
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("/*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    });
+    // app.get("/*", function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    // });
   }
-  
+  /*
   else {
     app.use(express.static(path.join(__dirname, '/client/public')));
     app.get("/*", function(req, res) {
       res.sendFile(path.join(__dirname, "../client/public/index.html"));
     });
   }
-
+*/
 app.use(passport.initialize()); // initialize passport
 app.use(passport.session()); // persistent login sessions
 const routes = require ("./routes");
