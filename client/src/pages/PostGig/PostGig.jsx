daa2cd5b43fe05edd44eda1bd151fcd2505ab4b2
Sajeel Malik <sajeel23@berkeley.edu>
2018-12-16 20:35:21 -0800

    set up post gig page

--- client/src/pages/PostGig/PostGig.jsx
+++ client/src/pages/PostGig/PostGig.jsx
@@ -87,7 +87,7 @@ const styles = theme => ({
 });
 
 
-class NewsFeed extends Component {
+class PostGig extends Component {
     constructor(props) {
         super(props);
 
@@ -104,73 +104,35 @@ class NewsFeed extends Component {
 
     }
 
+
     componentDidMount() {
 
         API.getUser().then(res => {
-            console.log("newsfeed component mounting check", res.data);
             if (!res.data.user) {
+                this.setState({ loggedIn: false });
                 window.location.href = "/signin";
-                // } else if (res.data.user.userType === "venue") {
-                //     window.location.href = "/signin";
+            }
+            else {
+                console.log("Select Artist Mounting Check", res.data, res.data.user.id);
+                if (res.data.user.userType === "artist") {
+                    window.location.href = "/artist";
+                }
+                else if (res.data.user.userType === "venue") {
+                    this.setState({
+                        loggedIn: true,
+                        userId: res.data.user.id,
+                        name: res.data.user.name,
+                        userType: res.data.user.userType,
+                        genres: JSON.parse(res.data.user.genres),
+                    });
 
-            } else {
-
-                console.log("the user name is ", res.data.user.name);
-                this.setState({
-                    username: res.data.user.name,
-                    userType: res.data.user.userType
-                });
-
-                // this.setState({ pic:res.data.user.profilePic});
-
+                }
             }
         });
-
-        API.getNews()
-            .then(res => {
-                console.log("news is", res.data.data);
-                this.state.messages = res.data;
-            });
-
-        //Socket.io
-        // this.socket = io.connect('http://localhost:3001');
-        this.socket = io.connect();
-
-
-        this.socket.on('RECEIVE_MESSAGE', function (data) {
-            addMessage(data);
-        });
-
-        const addMessage = data => {
-            console.log(data);
-            this.setState({ messages: [...this.state.messages, data] });
-            console.log(this.state.messages);
-        };
-
-        this.sendMessage = ev => {
-            ev.preventDefault();
-            // first add it to the data base
-
-            API.saveNews({
-                author: this.state.username,
-                message: this.state.message
-            });
-
-            // then emit it to the world
-            this.socket.emit('SEND_MESSAGE', {
-                author: this.state.username,
-                message: this.state.message
-            });
-            this.setState({ message: '' });
-
-        };
-
-
     }
 
     render() {
         const { classes } = this.props;
-        console.log(this.state.messages);
 
         return (
             <div>
@@ -181,26 +143,8 @@ class NewsFeed extends Component {
                     <Navigate method={this.changeState} userType={this.state.userType} name={this.props.name} pic={this.props.pic} />
 
 
-                    <div className="message-box-wrap">
-                        <div className="send-message">
-                            {/* <TextField type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
-                        <br/> */}
-                            <TextField type="text" placeholder="Message" className="send-message-text-field" value={this.state.message} onChange={ev => this.setState({ message: ev.target.value })} />
-                            <br />
-                            <Button className="send-message-button" variant="contained"
-                                color="primary" onClick={this.sendMessage} color="primary">Send</Button>
-                        </div>
-                        <div className="message-box">
-                            {this.state.messages.map(message => {
-                                return (
-                                    <div className="message-item"><div className="small-profile-tile" style={{ backgroundImage: "url(" + {} + ")" }}></div><h3>{message.author} </h3><p> :  {message.message}</p></div>
-                                );
-                            })}
-                        </div>
-                        <div className="message-header">
-                            <h1>What's Going on In Tempo</h1>
-                            <p></p>
-                        </div>
+                    <div className="postgig-box-wrap">
+
                     </div>
 
                 </div>
@@ -211,4 +155,4 @@ class NewsFeed extends Component {
 
 }
 
-export default NewsFeed;
+export default PostGig;
